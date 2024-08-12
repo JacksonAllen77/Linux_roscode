41251697634263d9af7a8baf9c1153c5e29a89d6
ro <>
2024-08-13 07:00:37 +0800

    2024年08月13日07:00:33

--- urg/build/catkin_generated/setup_cached.sh
+++ urg/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ro/ROS/urg/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ro/ROS/urg/devel/lib:$LD_LIBRARY_PATH"
 export PWD='/home/ro/ROS/urg/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/ro/ROS/urg/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/ro/ROS/urg/src:$ROS_PACKAGE_PATH"
